3dc8753a117ed1bfdaa621b24df99bc61f9586cd
20127670 <82032762+NoNameNo1F@users.noreply.github.com>
2023-07-09 18:29:33 +0700

    updated: form display information Header of User current

--- Pages/Home/Home.cshtml.cs
+++ Pages/Home/Home.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using IPinfo;
+using IPinfo.Models;
 namespace Cryptography.Pages;
 
 public class IndexModel : PageModel
@@ -11,9 +12,40 @@ public class IndexModel : PageModel
     {
         _logger = logger;
     }
+    public IPResponse userIpinfo { get; set; }
+    public string GetOperatingSystem(string userAgent)
+    {
+        int start = userAgent.IndexOf('(') + 1;
+        int end = userAgent.IndexOf(')');
 
+        if (start >= 0 && end >= 0 && end > start)
+        {
+            return userAgent.Substring(start, end - start);
+        }
+
+        return string.Empty;
+    }
+
+    public string GetBrowser(string userAgent)
+    {
+        int start = userAgent.IndexOf(" Gecko") + 8;
+        int end = userAgent.IndexOf(' ', start);
+
+        if (start >= 6 && end >= 0 && end > start)
+        {
+            return userAgent.Substring(start, end - start);
+        }
+
+        return string.Empty;
+    }
     public void OnGet()
     {
-        
+        // Khởi tạo Ipinfo ip
+        string? token = WebApplication.CreateBuilder().Configuration.GetConnectionString("token-Ipinfo");
+        IPinfoClient ip = new IPinfoClient.Builder()
+                            .AccessToken(token)
+                            .Build();
+        IPResponse iPResponse = ip.IPApi.GetDetails();
+        userIpinfo = iPResponse;
     }
 }
